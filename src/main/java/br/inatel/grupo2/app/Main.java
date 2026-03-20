21c635ccc1662589a3c3095a8dc79d681dd9a6b0
Wellington Souza <wellingtonhdds27@gmail.com>
2026-03-20 15:46:30 -0300

    feat: adicionando a logica do pedra, papel e tesoura

--- src/main/java/br/inatel/grupo2/app/Main.java
+++ src/main/java/br/inatel/grupo2/app/Main.java
@@ -1,4 +1,14 @@
 package br.inatel.grupo2.app;
 
+import br.inatel.grupo2.controller.rockpaperscissors.RockPaperScissorsController;
+
 public class Main {
+
+    public static void main(String[] args) {
+        // Para jogar o Pedra, Papel e Tesoura execute a main normalmente sem usar o '.\mvnw.cmd javafx:run'
+
+        //RockPaperScissorsController game = new RockPaperScissorsController();
+        //game.play();
+
+    }
 }
