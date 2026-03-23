70152b1ced276b9479843293fe41a860d91f4401
armengol2 <rodrigo.armengol@ges.inatel.br>
2026-03-23 16:37:23 -0300

    Update: Começando testes

--- src/main/java/br/inatel/grupo2/app/Main.java
+++ src/main/java/br/inatel/grupo2/app/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Para jogar o Pedra, Papel e Tesoura execute a main normalmente sem usar o '.\mvnw.cmd javafx:run'
-
+         //Testes perfeitos
         //RockPaperScissorsController game = new RockPaperScissorsController();
         //game.play();
 
