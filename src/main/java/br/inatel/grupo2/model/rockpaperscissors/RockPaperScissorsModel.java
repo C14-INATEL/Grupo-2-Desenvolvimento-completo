5f7ae93ea960d63f8e5cb70b5da685e462768c57
Wellington Souza <wellingtonhdds27@gmail.com>
2026-04-09 20:52:27 -0300

    feat: adiciona validação de entradas no RockPaperScissorsModel

--- src/main/java/br/inatel/grupo2/model/rockpaperscissors/RockPaperScissorsModel.java
+++ src/main/java/br/inatel/grupo2/model/rockpaperscissors/RockPaperScissorsModel.java
@@ -12,19 +12,29 @@ public class RockPaperScissorsModel {
     }
 
     public String getResult(String player, String computer) {
-
-        if (player.equals(computer)) {
-            return "Empate";
+        if (player == null || computer == null) {
+            throw new IllegalArgumentException("Valores não podem ser nulos");
         }
+
+        player = player.toLowerCase().trim();
+        computer = computer.toLowerCase().trim();
+
+        if (!isValid(player) || !isValid(computer)) {
+            throw new IllegalArgumentException("Opção inválida");
+        }
+
+        if (player.equals(computer)) return "Empate";
 
         if (
                 (player.equals("pedra") && computer.equals("tesoura")) ||
                         (player.equals("papel") && computer.equals("pedra")) ||
                         (player.equals("tesoura") && computer.equals("papel"))
-        ) {
-            return "Jogador";
-        }
+        ) return "Jogador";
 
         return "Computador";
+    }
+
+    private boolean isValid(String op) {
+        return op.equals("pedra") || op.equals("papel") || op.equals("tesoura");
     }
 }
